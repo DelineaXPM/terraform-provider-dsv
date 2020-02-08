f68e2aa6bcfe23ebc62c3840ff8799dc5882b443
Adam Migus <adam@migus.org>
2020-02-07 23:08:46 -0500

    Expose SDK settings TLD and URLTemplate: fixes #1

--- provider.go
+++ provider.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"log"
+
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/thycotic/dsv-sdk-go/vault"
 )
 
 func providerConfig(d *schema.ResourceData) (interface{}, error) {
-	return vault.Configuration{
+	c := vault.Configuration{
 		Tenant: d.Get("tenant").(string),
 		Credentials: vault.ClientCredential{
 			ClientID:     d.Get("client_id").(string),
 			ClientSecret: d.Get("client_secret").(string),
 		},
-	}, nil
+	}
+
+	log.Printf("[DEBUG] tenant is set to %s", c.Tenant)
+
+	if tld, ok := d.GetOk("tld"); ok {
+		c.TLD = tld.(string)
+		log.Printf("[DEBUG] tld is set to %s", c.TLD)
+	}
+
+	if ut, ok := d.GetOk("url_template"); ok {
+		c.URLTemplate = ut.(string)
+		log.Printf("[DEBUG] url_template is set to %s", c.URLTemplate)
+	}
+	return c, nil
 }
 
 // Provider is a Terraform DataSource
@@ -39,8 +54,19 @@ func Provider() *schema.Provider {
 			},
 			"client_secret": {
 				Type:        schema.TypeString,
+				Sensitive:   true,
 				Required:    true,
 				Description: "The DevOps Secrets Vault client_secret",
+			},
+			"tld": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The DSV tenant top-level domain",
+			},
+			"url_template": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The DSV SDK API URL template",
 			},
 		},
 		ConfigureFunc: providerConfig,
